fb1c8fa776f603365fdad8ad0bcd956db0322a5b
Ogochukwu Ozotta <88560609+ogochukwu-ozotta@users.noreply.github.com>
2024-02-28 04:42:02 -0600

    testing out the iac pipeline6

--- terraform/install-tools.sh
+++ terraform/install-tools.sh
@@ -2,7 +2,7 @@
 # For Ubuntu 22.04
 sudo apt update -y
 
-# Installing Docker 
+# Install Docker 
 sudo apt install docker.io -y
 sudo usermod -aG docker $USER
 sudo systemctl enable --now docker 
@@ -10,13 +10,13 @@ sudo systemctl enable --now docker
 # Run Docker Container of Sonarqube
 docker run -d  --name sonar -p 9000:9000 sonarqube:lts-community
 
-# Installing AWS CLI
+# Install AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt install unzip -y
 unzip awscliv2.zip
 sudo ./aws/install
 
-# Installing Kubectl
+# Install Kubectl
 sudo apt update
 sudo apt install curl -y
 sudo curl -LO "https://dl.k8s.io/release/v1.28.4/bin/linux/amd64/kubectl"
@@ -25,25 +25,25 @@ sudo mv kubectl /usr/local/bin/
 kubectl version --client
 
 
-# Installing eksctl
+# Install eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
 
-# Installing Terraform
+# Install Terraform
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
 sudo apt install terraform -y
 
-# Installing Trivy
+# Install Trivy
 sudo apt-get install wget apt-transport-https gnupg lsb-release -y
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
 echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
 sudo apt update
 sudo apt install trivy -y
 
-# Installing Argo CD with Kubectl
+# Install Argo CD with Kubectl
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.4.7/manifests/install.yaml
 sudo apt install jq -y
@@ -58,11 +58,12 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 
-# Installing Prometheus
+# Install Prometheus
 helm install prometheus prometheus-community/prometheus --namespace monitoring --create-namespace
 
-# Installing Grafana
+# Install Grafana
 helm install grafana grafana/grafana --namespace monitoring --create-namespace
 
-# Installing ingress-nginx
+# Install ingress-nginx
 helm install ingress-nginx ingress-nginx/ingress-nginx
+
