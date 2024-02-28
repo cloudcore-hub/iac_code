9a99884cc345e5e3f0508096d252f4f2de3cb6c2
ogochukwu-ozotta <ogochukwu.ozotta@gmail.com>
2024-02-28 14:23:27 -0600

    deploying only the jumphost1

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,23 +1,8 @@
-output "cluster_name" {
-    description = "AWS EKS Cluster Name"
-    value = module.eks.cluster_name
-}
-
-output "cluster_endpoint" {
-    description = "Endpoint for AWS EKS"
-    value = module.eks.cluster_endpoint
-}
-
 output "region" {
-    description = "AWS EKS Cluster region"
+    description = "Jumphost Server region"
     value = var.region
-}
-
-output "cluster_security_group_id" {
-    description = "Security group ID for AWS EKS Cluster"
-    value = module.eks.cluster_security_group_id
 }
 output "jumphost_public_ip" {
   description = "Public IP address of the EC2 jumphost"
-  value       = aws_instance.jumphost.public_ip
+  value       = aws_instance.ec2.public_ip
 }
